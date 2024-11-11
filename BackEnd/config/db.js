5b83808ea1300456d824eb6ace7e33dff4b9390f
Mphilip1701 <69808104+Mphilip1701@users.noreply.github.com>
2024-11-11 13:21:33 -0600

    Revert back to the original credi

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'cis3368.c3o2cmgcysp1.us-east-2.rds.amazonaws.com',
+  host: 'bluerynodb-restored.cj02o08agyaa.us-east-2.rds.amazonaws.com',
   user: 'admin',
-  password: 'd0ntfa!lm3bro0',
-  database: 'BlueRynoProjectDB_Test',
+  password: 'admin123',
+  database: 'BlueRynoProjectDB',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
