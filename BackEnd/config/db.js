bd426a3b62236a75d5e76b5a879b896aac410996
Mphilip1701 <69808104+Mphilip1701@users.noreply.github.com>
2024-11-11 13:18:49 -0600

    Changes to the db.js

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'bluerynodb-restored.cj02o08agyaa.us-east-2.rds.amazonaws.com',
+  host: 'cis3368.c3o2cmgcysp1.us-east-2.rds.amazonaws.com',
   user: 'admin',
-  password: 'admin123',
-  database: 'BlueRynoProjectDB',
+  password: 'd0ntfa!lm3bro0',
+  database: 'BlueRynoProjectDB_Test',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
